ff5e3784b5a0bf6867295f73cdf173ba38b12a77
mohammedmusleh2040-blip <mohammedmusleh2040@gmail.com>
2026-07-03 03:14:21 +0300

    Add EmployeeStatement component with development notice

---
+++ src/pages/EmployeeStatement.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function EmployeeStatement() {
+  return (
+    <div style={{padding:20}}>
+      <h2>📄 كشف حساب الموظف</h2>
+      <p>قيد التطوير...</p>
+    </div>
+  );
+}
